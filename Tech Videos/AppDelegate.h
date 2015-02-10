e8fc9c7bc5ada9b6d8ce73ccbfa723f5766cd176
Edward Rezaimehr <erezaimehr@SREZAIMEHR-MBP.home>
2015-02-09 23:03:12 -0800

    Initial Commit

---
+++ Tech Videos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tech Videos
+//
+//  Created by Edward Rezaimehr on 2/9/15.
+//  Copyright (c) 2015 Edward Rezaimehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tech Videos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tech Videos
+//
+//  Created by Edward Rezaimehr on 2/9/15.
+//  Copyright (c) 2015 Edward Rezaimehr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
